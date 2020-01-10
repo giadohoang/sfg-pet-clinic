56ff29215b9eb970bfcde0d877c4fe013cdc2c2b
Gia Do <gia.dohoang@gmail.com>
2020-01-10 10:45:20 -0500

    Added Index page and controller. closes #8

---
+++ pet-clinic-web/src/main/java/com/springframework/sfgpetclinic/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package com.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    //When incoming requests are empty, or / or index or index.html, it will be mapped through this
+    @RequestMapping({"","/","index","index.html"})
+    public String index(){
+        return "index";
+    }
+}
